83475d0ff1281692d12f565ceafbd2c3f953d3de
Vinicius Romani <viniciusromani@tokenlab.com.br>
2018-02-15 18:36:15 -0200

    Integrates settings screen in flow

---
+++ ToDoList/Scenes/Settings/SettingsModels.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsModels.swift
+//  ToDoList
+//
+//  Created by Vinicius Romani on 15/02/18.
+//  Copyright (c) 2018 Vinicius Romani. All rights reserved.
+//
+
+import UIKit
+
+struct Settings {
+    
+}
